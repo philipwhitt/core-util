3ebf71605f4b939c0ca23a35b12cf45bb2ba8cfc
Philip Whitt <philip.whitt@bancvue.com>
2014-01-20 21:52:58 -0600

    Adding test mail class

---
+++ Core/Util/Mail/StubAdapter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\Util\Mail;
+
+class StubAdapter implements Driver {
+
+	public $sends = 0;
+
+	public $to;
+	public $fromName;
+	public $from;
+	public $subject;
+	public $body;
+
+	public function setTo($name, $address) {
+		$this->to = $address;
+	}
+	public function setFrom($name, $address) {
+		$this->fromName = $name;
+		$this->from     = $address;
+	}
+	public function setSubject($subject) {
+		$this->subject = $subject;
+	}
+	public function setBody($body) {
+		$this->body = $body;
+	}
+
+	public function send() {
+		$this->sends++;
+	}
+
+}
