f0ba6af1ade1fea92c8306008a38f8db99120529
Philip W <philip.whitt@sbcglobal.net>
2014-09-22 15:11:48 -0500

    Timezone issue

--- test/Mail/MandrillAdapterTest.php
+++ test/Mail/MandrillAdapterTest.php
@@ -10,6 +10,8 @@ class MandrillAdapterTest extends \PHPUnit_Framework_TestCase {
 	 * @expectedException Core\Util\Mail\Exception
 	 */
 	public function testInvalidAPIKeyException() {
+		date_default_timezone_set('America/Los_Angeles');
+
 		MailFactory::setDriver(new MandrillAdapter('1234'));
 
 		MailFactory::getDriver()->send();
